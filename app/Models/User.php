34d1db4b3adbf1d4911494e8f484a50285f07495
khalil9910 <khalilmosslih123@gmail.com>
2026-01-11 14:31:25 +0100

    chore: remove flutter api

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -14,7 +13,7 @@ class User extends Authenticatable implements MustVerifyEmail
     public const ROLE_EMPLOYE = 'employe';
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
